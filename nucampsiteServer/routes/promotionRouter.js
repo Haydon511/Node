efde7f508e500cd1623e4e7e77e4f0ae72f15278
Haydon <104402133+Haydon511@users.noreply.github.com>
2022-09-14 12:31:03 -0600

    Add files via upload

---
+++ nucampsiteServer/routes/promotionRouter.js
@@ -0,0 +1,76 @@
+const express = require('express'); 
+const Promotion = require('../models/promotion');
+const authenticate = require('../authenticate'); 
+
+const promotionRouter = express.Router(); 
+
+promotionRouter.route('/')
+.get((req, res, next) => {
+    Promotion.find()
+    .then(promotions => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions);
+    })
+    .catch(err => next(err));
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.create(req.body)
+    .then(promotion => {
+        console.log('Promotion Created ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+.put(authenticate.verifyUser, (req, res)=> {
+    res.statusCode = 403; 
+    res.end('Put operation not supported on /promotions'); 
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) =>{
+    Promotion.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));}); 
+
+promotionRouter.route('/:promotionId')
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+}) 
+.post(authenticate.verifyUser, (req, res) =>{
+    res.write(`Adding the promotion: ${req.params.promotionId}`) 
+    res.end(`Will add the promotion: ${req.body.name}
+    with description: ${req.body.description}`);
+}) 
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, { new: true })
+    .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err)); 
+}) 
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));});
+
+
+module.exports = promotionRouter; 
